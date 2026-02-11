bbca14551a855cd2995d50211109cf1cc938b036
Arthur <arthurki2047@gmail.com>
2026-02-11 05:10:08 +0000

    ADD THESE options to the buttom of the page  DMCA/Copyright Policy  Term

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import { BottomNav } from '@/components/bottom-nav';
+import { Footer } from '@/components/footer';
 
 export default function MainLayout({
   children,
@@ -9,6 +10,7 @@ export default function MainLayout({
     <div className="relative flex min-h-screen flex-col">
       <BottomNav />
       <main className="flex-1">{children}</main>
+      <Footer />
       {/* Padding at the bottom to prevent content from being obscured by the bottom nav */}
       <div className="h-24 md:hidden" />
     </div>
